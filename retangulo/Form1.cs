84e48fa6c081ab991d8f7f5d8a4b0a7428e47b36
goncalogomes <goncalogomes@WIN-GESFLHLR4EE>
2019-11-26 16:20:51 +0000

    Adicionar arquivos de projeto.

---
+++ retangulo/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace retangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        calcular r = new calcular();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            r.Altura = textBox1.Text;
+            r.Largura = textBox2.Text;
+            if (radioButton1.Checked) label4.Text = r.Area;
+            else if (radioButton2.Checked) label4.Text = r.Perimetro;
+            else if (radioButton3.Checked) label4.Text = r.Diagonal;
+        }
+    }
+}
---
+++ retangulo/calcular.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retangulo
+{
+    class calcular
+    {
+        private double _a, _l; // _a -> Altura _l -> Largura
+        public string Altura { set => _a = Convert.ToDouble(value); }
+        public string Largura { set => _l = Convert.ToDouble(value); }
+        public string Area => (_a * _l).ToString();
+        public string Perimetro => (2 * (_a + _l)).ToString();
+        public string Diagonal => (Math.Sqrt(_l * _l + _a * _a)).ToString();
+    }
+}
